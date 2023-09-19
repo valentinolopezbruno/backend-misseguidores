16da18231cc099c875577541bfe034b8853b1d2e
valentinolopezbruno <valentinolopezbruno@outlook.com>
2023-09-19 08:56:51 -0300

    arreglado4

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const { v4: uuidv4 } = require("uuid");
 const axios = require('axios')
 app.use(express.json());
 
-const corsOptions = {
-  origin: 'https://misseguidores.com',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Habilitar cookies y credenciales (si es necesario)
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://misseguidores.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use("/imagenes", express.static(path.join(__dirname, "./imagenes")));
 const { PrismaClient } = require("@prisma/client");
