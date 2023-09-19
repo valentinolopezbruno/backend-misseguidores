ec02fffad2e35a4c05e1f0d851bcbd31adf64407
valentinolopezbruno <valentinolopezbruno@outlook.com>
2023-09-19 08:40:11 -0300

    arreglado

--- index.js
+++ index.js
@@ -52,12 +52,6 @@ setInterval(() => {
   fechaActual = fecha.getMinutes();
 }, 3 * 60 * 1000);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://misseguidores.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 
 /* ---------------------------------CONFIG  PAYPAL --------------------------------------------------- */
