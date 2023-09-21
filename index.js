baf09c127e18b535680a25dfb71982c4631d70ec
valentinolopezbruno <valentinolopezbruno@outlook.com>
2023-09-21 10:58:10 -0300

    arreglando paypal 38

--- index.js
+++ index.js
@@ -390,7 +390,7 @@ app.post('/create-order-paypal', async (req, res) => {
     headers: {
         'Content-Type': 'application/json',
         'PayPal-Request-Id': '7b92603e-77ed-4896-8e78-5dea2050476a',
-        'Authorization': ` Bearer ${cliente_secret}` 
+        'Authorization': ` Bearer ${credencial[1].cliente_secret}` 
     },
     body: JSON.stringify({ "intent": "CAPTURE", "purchase_units": [ { "reference_id": "d9f80740-38f0-11e8-b467-0ed5f89f718b", "amount": { "currency_code": "USD", "value": "100.00" } } ], "payment_source": { "paypal": { "experience_context": { "payment_method_preference": "IMMEDIATE_PAYMENT_REQUIRED", "brand_name": "EXAMPLE INC", "locale": "en-US", "landing_page": "LOGIN", "shipping_preference": "SET_PROVIDED_ADDRESS", "user_action": "PAY_NOW", "return_url": "https://misseguidores.com/succes", "cancel_url": "https://misseguidores.com/failure" } } } })
 });
